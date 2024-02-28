58b9af86ccbe1d35d335c07881bf0868cf84928f
Replit user <>
2024-02-28 20:54:56 +0000

    Added compare_life_expectancy_to_seconds file

---
+++ computing_with_formulas/compare_life_expectancy_to_seconds.py
@@ -0,0 +1,8 @@
+life_expectency = 76.4 # years
+
+one_billion = 1000000000 # seconds
+seconds_in_a_year = 31536000 # seconds
+
+one_billion_seconds_to_years = one_billion / seconds_in_a_year
+
+print (f' One billion seconds is equal to {one_billion_seconds_to_years:.1f} years, which is less than the life expectancy of {life_expectency} years.')
