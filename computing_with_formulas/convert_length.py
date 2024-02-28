edc61c591c74714e21ee0587a66a74590ccf1802
Replit user <>
2024-02-28 20:56:04 +0000

    Added convert_length file

---
+++ computing_with_formulas/convert_length.py
@@ -0,0 +1,25 @@
+kilometers_to_campus = 27.84 # kilometers
+
+kilometers_to_inches = kilometers_to_campus * 39370.1 # inches
+kilometers_to_feet = kilometers_to_inches / 12
+kilometers_to_yards = kilometers_to_feet / 3 
+kilometers_to_miles = kilometers_to_yards /1760 
+
+print(f' Distance to inches is {kilometers_to_inches:.2f} inches')
+print(f' Distance to feet is {kilometers_to_feet:.2f} feet')
+print(f' Distance to yards is {kilometers_to_yards:.2f} yards')
+print(f' Distance to miles is {kilometers_to_miles:.2f} miles')
+
+
+
+kilometers_example = 0.640 # kilometers
+
+kilo_example_to_inches = kilometers_example * 39370.1
+kilo_example_to_feet = kilo_example_to_inches / 12 
+kilo_example_to_yards = kilo_example_to_feet / 3
+kilo_example_to_miles = kilo_example_to_yards /1760 
+
+print(f' Distance to inches is {kilo_example_to_inches:.2f} inches')
+print(f' Distance to feet is {kilo_example_to_feet:.2f} feet')
+print(f' Distance to yards is {kilo_example_to_yards:.2f} yards')
+print(f' Distance to miles is {kilo_example_to_miles:.4f} miles')
