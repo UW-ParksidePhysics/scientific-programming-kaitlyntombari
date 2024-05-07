e6268c31170a84ab21f6f6000d31558f56472543
Kaitlyn Tombari <158516683+kaitlyntombari@users.noreply.github.com>
2024-05-07 02:51:00 +0000

    Updated animate_ball_trajectories

--- array_computing_and_curve_plotting/animate_ball_trajectories.py
+++ array_computing_and_curve_plotting/animate_ball_trajectories.py
@@ -1,6 +1,6 @@
+from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import animation
 
 
 def x(t, angle, initial_velocity):
@@ -17,19 +17,39 @@ if __name__ == "__main__":
     g = 9.81  # m/s²
     times_of_flight = 2 * initial_conditions[:, 1] * np.sin(initial_conditions[:, 2]) / g
     maximum_heights = initial_conditions[:, 1] ** 2 * \
-    np.sin(initial_conditions[:, 2])**2 / (2 * g)
+        np.sin(initial_conditions[:, 2])**2 / (2 * g)
 
-
-    figure, axes = plt.subplots()
-    axes.set_ylim(0, maximum_heights.max() * 1.1)
-    axes.set_xlim(0, x(times_of_flight, initial_conditions[:, 2], \
-initial_conditions[:, 1]).max() * 1.1)
+    fig, ax = plt.subplots()
+    ax.set_ylim(0, maximum_heights.max() * 1.1)
+    ax.set_xlim(0, x(times_of_flight.max(), initial_conditions[:, 2].max(), \
+        initial_conditions[:, 1].max()) * 1.1)
     lines = []
-    for index, (maximum_height, color) in enumerate(zip(maximum_heights, colors)):
-        line, = axes.plot([], [], lw=2, color=color)  
+    for idx, (maximum_height, color) in enumerate(zip(maximum_heights, colors)):
+        line, = ax.plot([], [], lw=2, color=colors[idx])  
         lines.append(line)
-        axes.axhline(y=maximum_height, color=color)
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.show()
+        ax.axhline(y=maximum_height, color=color)
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+
     
+    def init():
+        for line in lines:
+            line.set_data([], [])
+        return lines
+
+    
+    def update(frame):
+        t = np.linspace(0, times_of_flight[frame], 1000)
+        for idx, (_, initial_velocity, angle) in \
+        enumerate(initial_conditions):
+            x_vals = x(t, angle, initial_velocity)
+            y_vals = y(t, angle, initial_velocity, g)
+            valid_indices = np.where(y_vals >= 0)
+            lines[idx].set_data(x_vals[valid_indices], y_vals[valid_indices])
+        return lines
+
+    
+    anim = FuncAnimation(fig, update, frames=len(times_of_flight), init_func=init, \
+    blit=True)
+
+    plt.show()
