073ed63c8a1403b8587edff9b152905ce19f67fc
Kaitlyn Tombari <158516683+kaitlyntombari@users.noreply.github.com>
2024-05-10 14:49:17 +0000

    modified final script and convert_units files

--- final_exam_review/final_exam/Tombari_PHYS241FinalScript.py
+++ final_exam_review/final_exam/Tombari_PHYS241FinalScript.py
@@ -1,21 +1,141 @@
 """
-
+Final code
 """
 
 __author__ = "Kaitlyn Tombari"
 
-import matplotlib.pyplot as plt
-import numpy as np
 import datetime
 
-from read_two_columns_text import read_two_columns_text
-from calculate_bivariate_statistics import calculate_bivariate_statistics
-from calculate_quadratic_fit import calculate_quadratic_fit
-from equations_of_state import fit_eos
-from convert_units import convert_units
+import matplotlib.pyplot as plt
+import numpy as np
 from annotate_plot import annotate_plot
+from calculate_bivariate_statistics import calculate_bivariate_statistics
+from calculate_lowest_eigenvectors import calculate_lowest_eigenvectors
+from calculate_quadratic_fit import calculate_quadratic_fit
+from convert_units import convert_units
+from equations_of_state import fit_eos
 from generate_matrix import generate_matrix
+from read_two_columns_text import read_two_columns_text
 
 
-def parse_file_name():
-  
+def parse_file_name(file_name):
+    """Function used to extract the chemical symbol, crystal symmetry symbol, and \
+    desity funcional exchange-correlation approximation acronym from a given data \
+    file's name."""
+    file_name_split = str.split(file_name, '-')
+    chemical_symbol = file_name_split[0]
+    crystal_symmetry_symbol = file_name_split[1]
+    functional_exchange_correlation_approximation = file_name_split[2]
+    return chemical_symbol, crystal_symmetry_symbol, \
+    functional_exchange_correlation_approximation
+
+
+def main():
+    """Part One: Fit an Equation of State."""
+    
+    # Parse the file name
+    file_name = 'final_exam_review/Pt.Fm-3m.GGA-PBE.volumes_energies.dat'
+    chemical_symbol, crystal_symmetry_symbol, \
+    functional_exchance_correlation_approximation = parse_file_name(file_name)
+    
+    # Read in the data into a NumPy array.
+    data = read_two_columns_text(file_name)
+
+    # Fit a quadratic polynomial to the data 
+    fit_coefficients = calculate_quadratic_fit(data)
+
+    # Pull out the statistics on the data set
+    mean_y, standard_deviation_y, minimum_x_value, maximum_x_value, minimum_y_value, \
+    maximum_y_value = calculate_bivariate_statistics(data)
+    
+    # Pass the fit quadratic coefficients and the data to the fit_eos function
+    eos_fit_curve, eos_parameters = fit_eos(data[:, 0], data[:, 1], fit_coefficients, \
+    eos='vinet')
+
+    # Convert the units of the data and fit
+    converted_volume = convert_units(1, 'cubic_bohr/atom', 'cubic_angstrom/atom')
+    converted_energy = convert_units(1, 'rydberg/atom', 'electron_volts/atom')
+    converted_bulk_modulus = convert_units(1, 'rydberg/cubic_bohr', 'gigapascals')
+
+    # Plot the data and the fit curve
+    plt.figure()
+    plt.plot(converted_volume, converted_energy, 'bo', label='Data Points')
+    plt.plot(converted_volume, eos_fit_curve, 'k-', label='Fit Curve')
+
+    # Set the axes limits
+    plt.xlim(min(data[0]) - 0.1 * (max(data[0]) - min(data[0])), max(data[0]) + 0.1 * \
+             (max(data[0]) - min(data[0])))
+    plt.ylim(min(data[1]) - 0.1 * (max(data[1]) - min(data[1])), max(data[1]) + 0.1 * \
+         (max(data[1]) - min(data[1])))
+
+    # Label the axes
+    plt.xlabel('Volume (Å^3/atom)')
+    plt.ylabel('Energy (eV/atom)')
+    plt.title(f'{chemical_symbol} Equation of State in DFT \
+    ({functional_exchance_correlation_approximation})')
+
+    # Annotate the plot
+    annotate_plot({'chemical_symbol': {'position': (0.1, 0.9)}})
+    annotate_plot({'crystal_symmetry_symbol': {'position':(0.5, 0.95), 'italic':True}})
+    annotate_plot({'K_0': {'text': f'K_0 = {converted_bulk_modulus:.1f} GPa', \
+    'position': (0.5, 0.9)}})
+    annotate_plot({'equilibrium_volume': {f'Equilibrium Volume: \
+    {np.min(converted_volume):.2f} Å^3/atom': {'position': (0.5, 0.85)}}})
+    annotate_plot({'E_0': {'text': f'E_0 = {converted_energy:.2f} eV/atom', \
+    'position': (0.5, 0.8)}})
+
+    # Sign and title the graph
+    annotate_plot({'created_by': {'text': f'Created by Kaitlyn Tombari on \
+    {datetime.datetime.now().isoformat()[:10]}', 'position': (0.1, 0.1)}})
+    plt.title(f'{functional_exchance_correlation_approximation} Equation of State for \
+    {chemical_symbol} in DFT {crystal_symmetry_symbol}')
+
+    # Display or save the plot
+    display_graph = True
+    if display_graph:
+        plt.show()
+    else:
+        plt.savefig('Tombari.{chemical_symbol}.{crystal_symmetry_symbol}.{functional_exchance_correlation_approximation}EquationOfState.png')
+
+    
+    """Part Two: Visualize Vectors in Space"""
+
+    # Generate the matrix with given data
+    matrix = generate_matrix(minimum_x_value, maximum_x_value,\
+    potential_name='sinusodial', number_of_dimensions=130, potential_parameter=3)
+
+    # Calculate the eigenvectors and eigenvalues of the matrix
+    eigenvalues, eigenvectors = calculate_lowest_eigenvectors(matrix)
+
+    # Generate the grid of spatial points
+    grid_points = np.linspace(-10, 10, 130)
+
+    # Plot the eigenvectors against the grid
+    plt.figure()
+    for i in [3, 4, 5]:
+        eigenvector = eigenvectors[i]
+        if np.any(eigenvector < 0):
+            eigenvector *= -1
+        plt.plot(grid_points, eigenvector, label=f'ψ{i+1}, E{i+1} = \
+        {eigenvalues[i]:.3f} a.u.')
+    plt.plot()
+    plt.xlabel('x [a.u.]')
+    plt.ylabel('ψ_n (x) [a.u.]')
+    plt.title(f'Select Wavefunctions for a {crystal_symmetry_symbol} Potential on a \
+    Spatial Grid of 130 Points')
+    plt.axhline(0, color='black', linestyle='-')
+    plt.legend()
+    annotate_plot({'created by': {'text': f'Created by Kaitlyn Tombari on \
+    {datetime.datetime.now().isoformat()}', 'position': (0.1, 0.1)}})
+             
+    # Display or save the plot
+display_graph = True
+if display_graph:
+    plt.show()
+else:
+    plt.savefig('Tombari.{chemical_symbol}.Eigenvector130.png')
+
+
+if __name__ == "__main__":
+    main()
+    
--- final_exam_review/final_exam/convert_units.py
+++ final_exam_review/final_exam/convert_units.py
@@ -1,8 +1,40 @@
 """
-Convert data from one unit to another
+Convert data from one unit to another.
 """
 
 __author__ = "Kaitlyn Tombari"
 
-def convert_units():
-  
+
+def convert_units(value, from_units, to_units):
+    """
+    Function used to convert data from one unit to another.
+    Parameters:
+        value (float): The value to be converted.
+        from_units (str): The units of the value to be converted from.
+        to_units (str): The units to convert the value to.
+    Returns:
+        float: The converted value.
+    """
+    # Define the conversion factors
+    conversion_factors = {
+      'cubic_bohr/atom': 0.14818471147216278, # cubib bohr per atom to cubic angstroms \
+      # per atom
+      'rydberg/atom': 13.605693122994, # rydberg per atom to electron volts per atom
+      'rydberg/cubic_bohr': 14710.507848260711, # rydberg per cubic bohr to gigapascals
+      'cubic_angstrom/atom': 1.0, # cubic angstroms per atom to cubic bohr per atom
+      'electron_volts/atom': 1.0, # electron volts per atom to rydberg per atom'
+      'gigapascals': 1.0 # gigapascals to rydberg per cubic bohr
+    }
+    
+    # Convert the units
+    converted_value =  value * conversion_factors[from_units] / conversion_factors\
+    [to_units]
+    
+    return converted_value
+
+
+if __name__ == "__main__":
+    print("1 cubic bohr per atom equals", convert_units(1, 'cubic_bohr/atom', 'cubic_angstrom/atom'), "cubic angstroms per atom")
+    print("1 rydberg per atom equals", convert_units(1, 'rydberg/atom', 'electron_volts/atom'), "electron volts per atom")
+    print("1 rydberg per cubic bohr equals", convert_units(1, 'rydberg/cubic_bohr', 'gigapascals'), "gigapascals")
+    
